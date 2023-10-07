6b7165d351bf95600b945d6b78532381f737c504
«Yelzhan» <bekturganov.yelzhan@mail.ru>
2023-10-07 16:40:15 +0600

    example010 zakonchili

--- Example010_MethodArray/Program.cs
+++ Example010_MethodArray/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int[] array = { 1, 12, 31, 4, 18, 15, 16, 17, 18 };
+
+int n = array.Length;
+int find = 18;
+
+int index = 0;
+
+while(index < n)
+{
+    if(array[index] == find)
+    {
+        System.Console.WriteLine(index);
+        break; // найти только первый элемент
+    }
+    index++;
+}
